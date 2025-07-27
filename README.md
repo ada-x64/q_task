f4a7d5d00d2f0d30033fd8d91049c2d5281e398a
ada mandala <phoenix.mandala@pm.me>
2025-07-27 16:30:03 -0500

    add tests

---
+++ README.md
@@ -0,0 +1,7 @@
+# q_task
+
+A microscopic plugin for polling the status of tasks in Bevy.
+
+| release | bevy version |
+| --- | --- |
+| 0.1 | 0.16 |
--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,7 @@
-use bevy::{
-    ecs::world::CommandQueue,
-    prelude::*,
-    tasks::{block_on, futures_lite::future, Task},
-};
+use bevy_app::prelude::*;
+use bevy_ecs::prelude::*;
+pub use bevy_ecs::world::{CommandQueue, World};
+use bevy_tasks::{Task, futures_lite::future, prelude::*};
 
 #[derive(Component)]
 pub struct TaskComponent(pub Task<CommandQueue>);
@@ -15,29 +14,61 @@ fn poll_tasks(mut commands: Commands, tasks: Query<&mut TaskComponent>) {
     }
 }
 
-/// Arguments: an async move lambda taking &mut CommandQueue as argument.
+/// Creates an asychronously executing task. When finished, may optionally send an event.
 ///
 /// Example usage:
-/// ```rust, ignore
-/// task!(async move |_q| { info!("do something") })(world)
+/// ```rust
+/// # use bevy_app::prelude::*;
+/// # use bevy_tasks::IoTaskPool;
+/// # use bevy_app::{ScheduleRunnerPlugin, TaskPoolPlugin};
+/// # use bevy_ecs::prelude::*;
+/// # use q_tasks::*;
+/// #
+/// # #[derive(Event, Default)]
+/// # pub struct MyEvent();
+/// #
+/// # let mut app = App::new();
+/// # app.add_plugins((
+/// #     TaskPoolPlugin::default(),
+/// #     ScheduleRunnerPlugin::default(),
+/// #     TaskPlugin,
+/// #  ));
+/// # let mut world = app.world_mut();
+/// let task = task!(
+///     IoTaskPool,
+///     MyEvent::default(), // (optional)
+///     async move |q: &mut CommandQueue| {
+///         // do some async stuff
+///         q.push(|world: &mut World| {
+///             // do some world mutation
+///         });
+///     });
+/// task(world);
 /// ```
 #[macro_export]
 macro_rules! task {
     ($pool_type:path, $block:expr) => {
-        (move |world: &mut World| {
+        task!(@inner $pool_type, $block)
+    };
+    ($pool_type:path, $event:expr, $block:expr) => {
+        task!(@inner $pool_type, $block, $event)
+    };
+    (@inner $pool_type:path, $block:expr $(, $event:expr)?)  => {
+        (move |world: &mut $crate::World| {
             let mut entity = world.spawn_empty();
             let id = entity.id();
             let task = <$pool_type>::get().spawn(async move {
-                let mut q = bevy::ecs::world::CommandQueue::default();
+                let mut q = $crate::CommandQueue::default();
                 ($block)(&mut q).await;
-                q.push(move |world: &mut World| {
+                q.push(move |world: &mut $crate::World| {
                     world.despawn(id);
+                    $(world.trigger($event))?
                 });
                 q
             });
             entity.insert($crate::TaskComponent(task));
         })
-    };
+    }
 }
 
 pub struct TaskPlugin;
@@ -47,12 +78,138 @@ impl Plugin for TaskPlugin {
     }
 }
 
-#[test]
-fn test() {
-    let mut app = App::new();
-    let i = 0;
-    app.add_plugins((MinimalPlugins, TaskPlugin))
-        .add_systems(Update, move |world: &mut World| {
-            task!(bevy::tasks::IoTaskPool, async move |_q| { println!("{i}") })(world)
-        });
+#[cfg(test)]
+mod test {
+    use super::*;
+    use bevy::platform::time::Instant;
+    use bevy::prelude::*;
+    use bevy::{app::ScheduleRunnerPlugin, log::LogPlugin};
+    use std::{marker::PhantomData, time::Duration};
+
+    #[derive(Event)]
+    struct Ran<T>(PhantomData<T>);
+    impl<T> Default for Ran<T> {
+        fn default() -> Self {
+            Self(PhantomData)
+        }
+    }
+
+    #[derive(Resource, Default, PartialEq, Debug)]
+    struct TestResults {
+        io_task_pool: bool,
+        io_task_pool_observer: bool,
+        compute_task_pool: bool,
+        compute_task_pool_observer: bool,
+        async_task_pool: bool,
+        async_task_pool_observer: bool,
+    }
+
+    #[test]
+    fn test_io() {
+        let mut app = App::new();
+        app.add_plugins((
+            TaskPoolPlugin::default(),
+            ScheduleRunnerPlugin::default(),
+            TaskPlugin,
+            LogPlugin {
+                filter: "debug".to_string(),
+                ..Default::default()
+            },
+        ))
+        .init_resource::<TestResults>()
+        .add_event::<Ran<IoTaskPool>>()
+        .add_event::<Ran<ComputeTaskPool>>()
+        .add_event::<Ran<AsyncComputeTaskPool>>()
+        .add_systems(Startup, move |world: &mut World| {
+            task!(
+                ComputeTaskPool,
+                Ran::<IoTaskPool>::default(),
+                async move |q: &mut CommandQueue| {
+                    debug!("In IoTaskPool");
+                    q.push(|world: &mut World| {
+                        world.resource_mut::<TestResults>().io_task_pool = true;
+                    })
+                }
+            )(world);
+            task!(
+                ComputeTaskPool,
+                Ran::<ComputeTaskPool>::default(),
+                async move |q: &mut CommandQueue| {
+                    debug!("In ComputeTaskPool");
+                    q.push(|world: &mut World| {
+                        world.resource_mut::<TestResults>().compute_task_pool = true;
+                    })
+                }
+            )(world);
+            task!(
+                AsyncComputeTaskPool,
+                Ran::<AsyncComputeTaskPool>::default(),
+                async move |q: &mut CommandQueue| {
+                    // busy-wait 1 sec to test it works
+                    debug!("In AsyncComputeTaskPool");
+                    let start = Instant::now();
+                    while Instant::now().duration_since(start) <= Duration::from_secs(1) {}
+                    debug!("...AsyncComputeTaskPool DONE");
+                    q.push(|world: &mut World| {
+                        world.resource_mut::<TestResults>().async_task_pool = true;
+                    })
+                }
+            )(world);
+        })
+        .add_observer(
+            |_: Trigger<Ran<IoTaskPool>>, mut res: ResMut<TestResults>| {
+                res.io_task_pool_observer = true;
+            },
+        )
+        .add_observer(
+            |_: Trigger<Ran<ComputeTaskPool>>, mut res: ResMut<TestResults>| {
+                res.compute_task_pool_observer = true;
+            },
+        )
+        .add_observer(
+            |_: Trigger<Ran<AsyncComputeTaskPool>>, mut res: ResMut<TestResults>| {
+                res.async_task_pool_observer = true;
+            },
+        );
+        app.update();
+        let res = app
+            .world_mut()
+            .get_resource::<TestResults>()
+            .expect("TestResults");
+        assert_eq!(
+            *res,
+            TestResults {
+                io_task_pool: true,
+                io_task_pool_observer: true,
+                compute_task_pool: true,
+                compute_task_pool_observer: true,
+                async_task_pool: false,
+                async_task_pool_observer: false,
+            }
+        );
+        // Observers and the entity for polling the async compute task.
+        assert_eq!(app.world_mut().entities().used_count(), 4);
+        // busy wait...
+        let start = Instant::now();
+        while Instant::now().duration_since(start) <= Duration::from_secs(2) {}
+        // ... update again
+        app.update();
+        let res = app
+            .world_mut()
+            .get_resource::<TestResults>()
+            .expect("TestResults");
+        assert_eq!(
+            *res,
+            TestResults {
+                io_task_pool: true,
+                io_task_pool_observer: true,
+                compute_task_pool: true,
+                compute_task_pool_observer: true,
+                async_task_pool: true,
+                async_task_pool_observer: true,
+            }
+        );
+        // Should only contain the observers.
+        assert_eq!(app.world_mut().entities().used_count(), 3);
+    }
 }
